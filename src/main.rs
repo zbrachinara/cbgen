8a993a65ac75c474801cdf22bd6ee38fa789cb94
zbrachinara <zbrachinara@gmail.com>
2021-07-27 18:56:48 -0400

    add panic logging

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     simple_logging::log_to_file("cbgen.log", LevelFilter::max()).unwrap_or_else(|_| {
         simple_logging::log_to(std::io::stdout(), LevelFilter::max());
     });
+    log_panics::init();
 
     // let f = File::open("test.0.0.mca").unwrap_or_else(|_| {
     //     println!("Got the wrong path");
